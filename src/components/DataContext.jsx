19c759e6061f88cbaec399f13fa86b986bbd16a7
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-08-08 16:30:24 +0330

    bugfix

--- src/components/DataContext.jsx
+++ src/components/DataContext.jsx
@@ -4,25 +4,7 @@ export const DataContext = createContext({ data: [] });
 
 const DataContextProvider = ({ children }) => {
   const [error,setError] = useState([])
-  const [data, setData] = useState([
-  // {
-  //   "title": "Temperature readings",
-  //   "data": [
-  //     [0, 22],
-  //     [1, 23],
-  //     [2, 21]
-  //   ]
-  // },
-  // {
-  //   "title": "Sensor comparisons",
-  //   "data": [
-  //     [0, [5, 8, 6]],
-  //     [1, [6, 9, 7]],
-  //     [2, [4, 7, 5]]
-  //   ]
-  // }
-]
-);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
